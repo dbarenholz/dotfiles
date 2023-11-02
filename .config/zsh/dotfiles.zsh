bf7a0bcafa8a955dc3d32dd6291fbe3dca796915
Daniel Barenholz <hello@dbarenholz.com>
2023-11-02 20:30:32 +0100

    Update dotfiles

--- .config/zsh/dotfiles.zsh
+++ .config/zsh/dotfiles.zsh
@@ -1,16 +1,19 @@
 dotfiles() {
-  # TODO: Check for 
-  #    cp
-  #    ln
-  #    dirname
-  #    realpath
+  REQUIRED_TOOLS=(cp ln dirname realpath git)
+  for tool in ${REQUIRED_TOOLS[@]}; do
+    if ! [[ -x "$(command -v $tool)" ]]; then
+      echo "$tool could not be found"
+      return
+    fi
+  done
 
-  # Where dotfiles are stored
   readonly DOTFILES="/home/dan/.dotfiles"
+  if ! [[ -w "$DOTFILES" ]]; then
+    echo "You do not have write access to the dotfiles directory: $DOTFILES"
+    return
+  fi
 
-  # TODO: Check for being able to read DOTFILES directory
-
-  # Print some usage if you just run it: TODO: replace with fancy menu
+  # TODO: replace with fancy menu
   if [[ $# -lt 1 ]]; then
     echo "Usage: dotfiles <task> <optional arguments>"
     echo "       dotfiles add file.ext"
@@ -19,24 +22,33 @@ dotfiles() {
     return
   fi
 
-  # Actual functionality
+  VALID_TASKS=("a" "add" "u" "update")
   readonly task=${1:?"Action must be specified"}
+  if ! [[ ${VALID_TASKS[@]} =~ $task ]]; then
+    echo "Invalid task given: $task."
+    return
+  fi
+
   if [[ "$task" -eq "add" || "$task" -eq "a" ]]; then
-    # dotfiles add <list of files or folders>
     for arg in "${@:2}"; do
-      # Extract directories to make, and make them
       DIRS="$(dirname $(realpath --relative-to=$HOME $(pwd)/$arg))"
       mkdir -p $DOTFILES/$DIRS
 
-      # folder: use cp -ali
       if [[ -d "$arg" ]]; then
         cp -al $(pwd)/$arg $DOTFILES/$DIRS
 
-      # file: use ln -ivt
       elif [[ -f "$arg" ]]; then
         ln -ivt $DOTFILES/$DIRS $arg
       else
       fi
     done
+  fi
+
+  if [[ "$task" -eq "update" || "$task" -eq "u" ]]; then
+    echo "Pushing to git"
+    git -C $DOTFILES add .
+    git -C $DOTFILES commit -S -m "Update dotfiles"
+    git -C $DOTFILES push
+    cd -
   fi
 }
