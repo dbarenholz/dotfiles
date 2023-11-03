9295a0acd7af5921f7be937bc89cf2f3c265717d
Daniel Barenholz <hello@dbarenholz.com>
2023-11-03 19:59:55 +0100

    nvim: add LSP for lua

---
+++ .config/nvim/README.md
@@ -0,0 +1,29 @@
+# Neovim
+
+TODO: Notes about setup.
+
+## Language Servers
+
+The way to add new language servers is split into two items:
+
+### Install the language server on the machine
+
+Because I'm on an immutable OS, this is different from usual.
+
+1. Enter `dev` distrobox.
+2. Install the language server on the system.
+     ex: `sudo pacman -S lua-language-server`
+3. Export the binary.
+     ex: `distrobox-export --bin /usr/sbin/lua-language-server`
+
+
+### Tell neovim to use the language server
+
+1. Open `https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md`
+2. Open `~/.config/nvim/lua/plugins/lsp-config.lua`.
+3. In the table starting at line 3: add the name of LSP (note the comma)
+     ex: `"  lua_ls",`
+4. Add the language server configuration blob. (Try to link to docs too)
+     ex: `configs.lua_ls = { ... }`
+
+That _should_ be all for language servers.
---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,51 @@
+-- setup inspired by pseudometa; see:
+-- https://github.com/chrisgrieser/.config/blob/main/nvim/lua/plugins/lsp-config.lua
+
+vim.g.lsps = {
+  "lua_ls",
+}
+
+
+-- table<string, config>
+local configs = {}
+for _,lsp in pairs(vim.g.lsps) do
+  configs[lsp] = {}
+end
+
+
+
+-- DOCS https://luals.github.io/wiki/settings/
+configs.lua_ls = {
+  settings = {
+    Lua = {
+      completion = {
+	callSnippet = "Replace",
+	keywordSnippet = "Replace",
+	displayContext = 6,
+	showWord = "Disable",  -- don't suggest common words as fallback
+	postfix = ".",         -- useful for `table.insert` and the like
+      },
+      diagnostics = {
+	globals = { "vim" },   -- when contributing to nvim plugins missing a .luarc.json
+      },
+      hint = {
+	enable = true,         -- enabled inlay hints
+	setType = true,
+	arrayIndex = "Disable",
+      },
+      -- FIX https://github.com/sumneko/lua-language-server/issues/679#issuecomment-925524834
+      workspace = { checkThirdParty = false },
+    },
+  },
+}
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      for lsp, config in pairs(configs) do
+	require("lspconfig")[lsp].setup(config)
+      end
+    end
+  }
+}
