3b386a23a3909f2c0778581e893dd0b6d21ce602
Daniel Barenholz <hello@dbarenholz.com>
2023-11-07 20:32:04 +0100

    ssh.zsh: hide output

--- .config/zsh/ssh.zsh
+++ .config/zsh/ssh.zsh
@@ -2,9 +2,9 @@
 SSH_ENV="$HOME/.ssh/agent-environment"
 
 function start_agent() {
-    echo "Initialising new SSH agent..."
+    # echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    echo "succeeded"
+    # echo "succeeded"
     chmod 600 "${SSH_ENV}"
     source "${SSH_ENV}" > /dev/null
     /usr/bin/ssh-add;
