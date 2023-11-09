ee6de56a264e6098fc027ab507379d721deb5f52
Daniel Barenholz <hello@dbarenholz.com>
2023-11-09 17:32:55 +0100

    nvim: Remove column limit

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -8,10 +8,3 @@ vim.o.undofile = true    -- save undo history through multiple sessions
 vim.o.shiftwidth = 2     -- pressing tab puts this many spaces instead
 vim.o.expandtabs = true  -- expand tabs to spaces
 vim.o.tabsize = 8        -- a \t character is 8 spaces
-
-vim.o.textwidth = 80     -- limit column at 80 characters
-vim.o.colorcolumn = '+1' -- highlight when you go over 80
-vim.cmd.highlight(       -- show black column add color column
- { "ColorColumn", "ctermbg=black" }
-)
-
