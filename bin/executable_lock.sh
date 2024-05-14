fb58fdab04b867b9e8b5f064efdcf0ff4c7ec90a
Daniel Barenholz <hello@dbarenholz.com>
2024-05-14 11:35:44 +0200

    lock.sh: fix for multiple monitors

--- bin/executable_lock.sh
+++ bin/executable_lock.sh
@@ -2,23 +2,25 @@
 
 ## Locking script that sets a blurred lockscreen with optional text
 # Uses grim, swaylock and imagemagick.
-# Usage: 
+# Usage:
 #   lock.sh        --> locks the screen with a blurred image
 #   lock.sh "text" --> locks the screen with a blurred image, with text on top
 
-grim "$HOME/.swaylock/img"
+IMG="$HOME/tmp/swaylock.img"
 
-convert "$HOME/.swaylock/img" \
+grim -o "$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')" "$IMG"
+
+convert "$IMG" \
   -scale 10% \
   -blur 0x2.5 \
-  -resize 1000% "$HOME/.swaylock/img"
+  -resize 1000% "$IMG"
 
-[[ "$1" ]] && convert "$HOME/.swaylock/img" \
+[[ "$1" ]] && convert "$IMG" \
   -gravity North \
   -pointsize 40 \
   -fill white \
   -font "MesloLGS-NF-Regular" \
-  -annotate +0+100 "$1" "$HOME/.swaylock/img"
+  -annotate +0+100 "$1" "$IMG"
 
 # Lock the screen with the blurred image
-exec swaylock --daemonize --ignore-empty-password --image="$HOME/.swaylock/img"
+exec swaylock --daemonize --ignore-empty-password --image="$IMG"
